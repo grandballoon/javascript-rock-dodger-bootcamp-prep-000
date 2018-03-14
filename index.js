95f8b7a566f5c256544a8609151981e712b1768f
Luke Conley <luke.sovern.conley@gmail.com>
2018-03-14 16:31:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ if (e.which == 39) {
 
 function moveDodgerLeft() {
 
-  var left = DODGER.style.left;
+  var left = positionToInteger(DODGER.style.left);
 DODGER.style.left = `${left += 4}px`
 
 }
