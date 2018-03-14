ee19cc8c5da9bcb25ad2a4adedae8552c15a53ad
Luke Conley <luke.sovern.conley@gmail.com>
2018-03-14 17:01:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ DODGER.style.left = `${left -= 4}px`
 function moveDodgerRight() {
 
   var left = positionToInteger(DODGER.style.left);
-  if (left < 400){
+  if (left < 396){
 DODGER.style.left = `${left += 4}px`
 }
 }
